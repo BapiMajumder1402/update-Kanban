39889fe7f307a07cc5507919ee527dac5d2f8728
robin <rcgaur84@gmail.com>
2023-05-10 13:12:58 +0530

    ReduxSetup

---
+++ karban/src/redux/toDoSlice.js
@@ -0,0 +1,18 @@
+import {createSlice} from '@reduxjs/toolkit'
+import {v4 as uuid} from 'uuid'
+const initialState={
+    value:[]
+}
+
+export const toDoSlice=createSlice({
+    name:"toDo", 
+    initialState,
+    reducers:{
+        addTask: (state, action)=>{
+            state.value.push({id:uuid(), task: action.payload, completed: false})
+        }
+    }
+})
+
+export const {addTask}=toDoSlice.actions
+export default toDoSlice.reducer
