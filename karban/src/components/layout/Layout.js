04dc6e0af1aaf2f61e4dd9e5361bc53791d315f1
Janammjay <janammjayk6@gmail.com>
2023-05-12 16:50:32 +0530

    design complete

--- karban/src/components/layout/Layout.js
+++ karban/src/components/layout/Layout.js
@@ -1,31 +1,18 @@
 import React from "react";
 import style from "./Layout.module.css";
-
-import { AiOutlinePlus } from "react-icons/ai";
 import Details from "../details/Details";
-
 import Navbar from "../navbar/Navbar";
 import Board from "../../board/Board";
 import Editable from "../../editable/Editable";
 
 function Layout() {
+  // const [boards,setBoard] = useState([{
+
+  // }])
   return (
     <div className={style.mainLayout}>
       <div className={style.image}>
         <Navbar />
-
-        {/* <div>
-          <button className={style.list}>
-            <span className={style.placeholder}>
-              <span className={style.addIcon}>
-              <AiOutlinePlus />
-              </span>
-              Add a List
-            </span>
-           
-           
-          </button>
-          </div> */}
         <div className={style.outer_board}>
           <div className={style.inner_board}>
             <Board />
